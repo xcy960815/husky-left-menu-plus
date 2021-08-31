c7b78b42b7469ee971e74a765242da5af0d9c563
xuchongyu <1064207592@qq.com>
2021-08-31 15:19:59 +0800

    fix bug

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 // @ts-ignore
 import App from './App.vue'
 import menu from '../dist/index.esm.js'
-import '../demo/index.css'
+import '../dist/index.css'
 import 'element-plus/lib/theme-chalk/index.css'
 const app = createApp(App)
 app.use(menu)
